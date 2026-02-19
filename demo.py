f9a687e12b3d1510cf7fb7a47b872032043f60cf
HAISTR <rohitnagtode3@gmail.com>
2026-02-19 10:17:37 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -1,10 +1,6 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-logging.info("Logging system successfully initialized!")
 
-try:
-    a = 2/0
-except Exception as e:
-    raise USvisaException(e, sys)
+obj = TrainPipeline()
+
+obj.run_pipeline()
